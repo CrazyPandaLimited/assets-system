5333ea128b3ba1a4f64a5efff9412d4447faea0f
Evgeniy Mineev <e.mineev@crazypanda.ru>
2021-08-09 10:43:58 +0400

    dispose webrequest after finish

--- Runtime/Processors/LoadProcessors/AbstractWebRequestLoadProcessor.cs
+++ Runtime/Processors/LoadProcessors/AbstractWebRequestLoadProcessor.cs
@@ -31,18 +31,25 @@ namespace CrazyPanda.UnityCore.AssetsSystem.Processors
 
         protected override void OnLoadingProgressUpdated( UrlLoadingRequest body, float currentProgress ) => body.ProgressTracker.ReportProgress( currentProgress );
 
-        protected override void OnLoadingCompleted( RequestProcessorData data ) => data.Body.ProgressTracker.ReportProgress( FinalProgress );
+        protected override void OnLoadingCompleted( RequestProcessorData data )
+        {
+            data.Body.ProgressTracker.ReportProgress( FinalProgress );
+            data.RequestLoadingOperation.webRequest.Dispose();
+        }
 
         protected override bool LoadingFinishedWithoutErrors( RequestProcessorData data ) => RequestFinishedWithoutErrors( data.RequestLoadingOperation.webRequest, data.Header, data.Body );
 
-        protected override void OnOperationCancelled( RequestProcessorData data ) => data.RequestLoadingOperation.webRequest.Dispose(); 
-
+        protected override void OnErrorLoading( RequestProcessorData data )
+        {
+            data.RequestLoadingOperation.webRequest.Dispose(); 
+            base.OnErrorLoading( data );
+        }
 
         private void BuildAndSendWebRequest( UnityWebRequest webRequest, MessageHeader header, UrlLoadingRequest body )
         {
             webRequest.downloadHandler = webRequest.downloadHandler ?? new DownloadHandlerBuffer();
             ConfigureWebRequest( webRequest, _webRequestSettings );
-
+            
 #if UNITY_2017_2_OR_NEWER
             var webRequestInProgressTask = webRequest.SendWebRequest();
 #else
